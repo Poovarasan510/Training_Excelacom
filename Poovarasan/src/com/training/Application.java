8fdc04a63fdf5fbe4c005f3d8b1e7f4c10782d5c
Poovarasan.P <Poovarasan.P@EXIND3010-04.allfon.in>
2022-04-06 23:33:15 -0700

    eighrt

---
+++ Poovarasan/src/com/training/Application.java
@@ -0,0 +1,15 @@
+package com.training;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Greeting obj=new Greeting();
+System.out.println(obj.getMessage());
+System.out.println(Greeting.getInfo());
+System.out.println(obj.getMessage());
+System.out.println(obj.getInfo());
+
+	}
+
+}
---
+++ Poovarasan/src/com/training/Greeting.java
@@ -0,0 +1,13 @@
+package com.training;
+
+public class Greeting {
+
+	public String getMessage()
+	{
+		return "welcome";
+	}
+	public static String getInfo()
+	{
+		return "hello from static method";
+	}
+}
