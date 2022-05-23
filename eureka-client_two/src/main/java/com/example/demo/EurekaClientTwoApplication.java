30bb7817ca5bc6f394c3533aa87804ac963a2774
Poovarasan.P <Poovarasan.P@EXIND3010-04.allfon.in>
2022-05-23 03:30:42 -0700

    eureka server registry

--- eureka-client_two/src/main/java/com/example/demo/EurekaClientTwoApplication.java
+++ eureka-client_two/src/main/java/com/example/demo/EurekaClientTwoApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,6 +13,12 @@ public class EurekaClientTwoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaClientTwoApplication.class, args);
+	}
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate template() {
+		return new RestTemplate();
 	}
 	
 	
